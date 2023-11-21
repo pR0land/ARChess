7417a19284f52aa189151c665b3abff5fa697340
kjulsg21 <100912975+kjulsg21@users.noreply.github.com>
2023-11-21 04:59:14 +0100

    Added comments to ARChessboardManager script

--- NewARChessProject/Assets/Scripts/ARChessboardManager.cs
+++ NewARChessProject/Assets/Scripts/ARChessboardManager.cs
@@ -7,39 +7,44 @@ using System.Collections.Generic;
 public class ARChessboardManager : MonoBehaviour
 {
     [SerializeField]
-    private ARPlaneManager arPlaneManager;
+    private ARPlaneManager arPlaneManager; // Reference to the ARPlaneManager
     [SerializeField]
-    private ARRaycastManager raycastManager;
+    private ARRaycastManager raycastManager; // Reference to the ARRaycastManager
     [SerializeField]
-    private GameObject chessboardPrefab;
+    private GameObject chessboardPrefab; // Reference to the chessboard prefab
     
-    private GameObject placedChessboard;
-    private bool isChessboardPlaced = false;
+    private bool _isChessboardPlaced; // Bool for checking if the chessboard has been placed
 
     void Update()
     {
-        if (!isChessboardPlaced)
+        // If the chessboard has not been placed, try to place it
+        if (!_isChessboardPlaced)
         {
+            // Check if the user has touched the screen
             if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
             {
+                // Try to place the chessboard
                 TryPlaceChessboard(Input.GetTouch(0).position);
             }
         }
     }
-
+    
+    // Method for placing the chessboard
     private void TryPlaceChessboard(Vector2 touchPosition)
     {
         List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        
+        // Check if the user has touched a plane
         if (raycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
         {
-            Pose hitPose = hits[0].pose;
-            placedChessboard = Instantiate(chessboardPrefab, hitPose.position, hitPose.rotation);
-            isChessboardPlaced = true;
+            Pose hitPose = hits[0].pose; // Get the pose of the hit
+            Instantiate(chessboardPrefab, hitPose.position, hitPose.rotation); // Instantiate the chessboard
+            _isChessboardPlaced = true; // Set the _isChessboardPlaced bool to true
 
             // Once the chessboard has been placed, disable the visualization of all planes
             foreach (var plane in arPlaneManager.trackables)
             {
-                plane.gameObject.SetActive(false);
+                plane.gameObject.SetActive(false); // Disable plane
             }
             
             arPlaneManager.enabled = false; // Disable the ARPlaneManager after the chessboard has been placed
